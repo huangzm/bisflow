b9193c4e62da2d98297fa7b8b87b1c024ec7b8ca
hzm <370699717@qq.com>
2016-04-02 13:11:13 +0800

    init

---
+++ src/main/java/com/hh/BisflowApplication.java
@@ -0,0 +1,12 @@
+package com.hh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BisflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BisflowApplication.class, args);
+	}
+}
